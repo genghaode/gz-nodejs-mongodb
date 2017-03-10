e19913552625ce4f99d73b8db919f49c4f5b989e
genghaode <genghaode@126.com>
2017-03-10 10:00:42 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,20 @@
+# 十九点项目，健康养生资讯站
+- wap站域名：m.shijiudian.com 开发中
+- pc站域名：www.shijiudain.com 未开发
+- app：未开发
+- 后台内容管理系统：未开发
+
+## 技术栈
+- 部署：centos7.2+nginx+pm2
+- 后端：nodejs+express+mongodb
+- 前端：react+react-router+redux+antd
+- 移动端：react-native
+
+## 开发过程记录
+- centos部署nodejs+mongodb+nginx
+- 本地使用express初始化项目，在使用前端脚手架初始化前端项目拷贝到public目录下
+- 简单配置好数据接口
+- 提交到github
+- 
+
+
---
+++ routes/api.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+
+var bannerData = {
+  myData: [{
+    id: 0,
+    url: '',
+    img: '/assets/images/001.png'
+  }, {
+    id: 1,
+    url: '',
+    img: '/assets/images/001.png'
+  }, {
+    id: 2,
+    url: '',
+    img: '/assets/images/001.png'
+  }, {
+    id: 3,
+    url: '',
+    img: '/assets/images/001.png'
+  }, {
+    id: 4,
+    url: '',
+    img: '/assets/images/001.png'
+  }, ]
+}
+router.get('/getBanner', function(req, res, next) {
+  res.json(bannerData);
+});
+
+
+router.get('/getItemList', function(req, res, next) {
+  var pageNum = req.query.pageNum
+
+  var itemListData = { myData: [], status: pageNum > 5 ? false : true }
+  for (var i = 0; i < 10; i++) {
+    itemListData.myData.push({
+      'title': pageNum + 'title' + i,
+      'fowllerNum': i,
+      'img': '/assets/images/002.png',
+      'time': '2017-01-01 08:30',
+      'id': (pageNum - 1) * 10 + i
+    })
+  }
+
+
+  res.json(itemListData);
+
+});
+
+router.post('/postLogin', function(req, res) {
+  if (req.body.name == 'gengzhuo' && req.body.pwd == '111111') {
+    res.json({ "myData": { 'status': true } })
+  } else {
+    res.json({ "myData": { 'status': false } })
+  }
+});
+
+module.exports = router;
