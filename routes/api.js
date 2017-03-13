80109daece680ae8cdc3031a47f9950024ebbbf6
genghaode <genghaode@126.com>
2017-03-13 14:31:45 +0800

    增加mongodb查询数据

--- routes/api.js
+++ routes/api.js
@@ -1,60 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-var bannerData = {
-  myData: [{
-    id: 0,
-    url: '',
-    img: '/assets/images/001.png'
-  }, {
-    id: 1,
-    url: '',
-    img: '/assets/images/001.png'
-  }, {
-    id: 2,
-    url: '',
-    img: '/assets/images/001.png'
-  }, {
-    id: 3,
-    url: '',
-    img: '/assets/images/001.png'
-  }, {
-    id: 4,
-    url: '',
-    img: '/assets/images/001.png'
-  }, ]
-}
+var Model = require('../db');
+var myUtil = require('../utils');
+
 router.get('/getBanner', function(req, res, next) {
-  req.session.user = 'gengzhuo';
-  res.json(bannerData);
+  Model('Banner').find({}).sort({ id: 1 }).exec(function(err, banners) {
+    if (banners.length) {
+      res.json({ myData: banners, status: true });
+    } else {
+      res.json({ myData: banners, status: false });
+    }
+  })
 });
 
-
 router.get('/getItemList', function(req, res, next) {
-  var pageNum = req.query.pageNum
-
-  var itemListData = { myData: [], status: pageNum > 5 ? false : true }
-  for (var i = 0; i < 10; i++) {
-    itemListData.myData.push({
-      'title': pageNum + 'title' + i,
-      'fowllerNum': i,
-      'img': '/assets/images/002.png',
-      'time': '2017-01-01 08:30',
-      'id': (pageNum - 1) * 10 + i
-    })
-  }
-
-
-  res.json(itemListData);
-
+  var pageNum = req.query.pageNum && req.query.pageNum > 0 ? parseInt(req.query.pageNum) : 1;
+  Model('Item').find({}).sort({ fowllerNum: -1 }).skip((pageNum - 1) * 10).limit(10).exec(function(err, items) {
+    if (items.length) {
+      res.json({ myData: items, status: true });
+    } else {
+      res.json({ myData: items, status: false });
+    }
+  })
 });
 
 router.post('/postLogin', function(req, res) {
-  if (req.body.name == 'gengzhuo' && req.body.pwd == '111111') {
-    res.json({ "myData": { 'status': true, 'user': req.session.user, 'name': 'geng' } })
-  } else {
-    res.json({ "myData": { 'status': false, 'user': req.session.user, 'name': 'geng' } })
-  }
+  var user = req.body;
+  user.pwd = myUtil.md5(user.pwd);
+
+  Model('User').findOne(user, function(err, user) {
+    console.log(user)
+    if (err) {
+      return res.json({ "myData": { status: false, error: '登录失败' } });
+    }
+    if (user) {
+      req.session.user = user;
+      return res.json({ "myData": { status: true, error: '登录成功' } });
+    } else {
+      return res.json({ "myData": { status: false, error: '用户名或密码不正确' } });
+    }
+  })
 });
 
 module.exports = router;
